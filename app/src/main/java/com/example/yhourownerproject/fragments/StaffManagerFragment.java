79ce429d0f40b230b589f43874796aa5bd28c320
Minh Trí Trần <tritranrpc@gmail.com>
2024-04-06 10:42:47 +0700

    fix staff list

--- app/src/main/java/com/example/yhourownerproject/fragments/StaffManagerFragment.java
+++ app/src/main/java/com/example/yhourownerproject/fragments/StaffManagerFragment.java
@@ -1,22 +1,42 @@
 package com.example.yhourownerproject.fragments;
 
-import android.content.Intent;
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.yhourownerproject.R;
-import com.example.yhourownerproject.activities.StaffListActivity;
+import com.example.yhourownerproject.adapter.StaffAdapter;
+import com.example.yhourownerproject.roles.Staff;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class StaffManagerFragment extends Fragment {
     private View mView;
-    Button staff_list_btn;
+
+    private RecyclerView recyclerView;
+    private StaffAdapter adapter;
+    private List<Staff> staffList = new ArrayList<>();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
 
     public StaffManagerFragment() {
@@ -40,15 +60,71 @@ public class StaffManagerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_staff_manager, container, false);
-        staff_list_btn = mView.findViewById(R.id.staff_list_btn);
+        recyclerView = mView.findViewById(R.id.recycler_view_staff_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new StaffAdapter(staffList);
+        recyclerView.setAdapter(adapter);
 
-        staff_list_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), StaffListActivity.class);
-                startActivity(intent);
-            }
-        });
+        loadDataFromFirebase();
+
         return mView;
+    }
+
+    private void loadDataFromFirebase() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        String userId = user.getUid();
+        if (user != null) {
+            firebaseDatabase.getReference().addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String ownerShopId = snapshot.child("User").child(userId).child("shopID").getValue(String.class);
+                    Log.d(TAG, "Owner Shop ID: " + ownerShopId);
+                    if (ownerShopId != null) {
+                        firebaseDatabase.getReference("User").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                                boolean shopFound = false;
+                                for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                                    String userKey = userSnapshot.getKey();
+                                    Integer userRole = userSnapshot.child("role").getValue(Integer.class);
+                                    String userName = userSnapshot.child("name").getValue(String.class);
+                                    String userId = userSnapshot.child("id").getValue(String.class);
+                                    Log.d(TAG, "User Key: " + userKey);
+                                    Log.d(TAG, "User Name: " + userName);
+                                    Log.d(TAG, "User Role: " + userRole);
+                                    if (userRole != null && userRole.equals(1)){
+                                        Staff staff = new Staff(userId, userName);
+                                        staffList.add(staff);
+                                        adapter.notifyDataSetChanged();
+                                    }
+
+
+                                }
+//                                if (!shopFound) {
+//                                    Toast.makeText(StaffListActivity.this, "Shop not a", Toast.LENGTH_SHORT).show();
+//                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }else {
+                        Toast.makeText(getContext(), "Shop not found", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        } else {
+            Toast.makeText(getContext(), "User not logged in", Toast.LENGTH_SHORT).show();
+        }
     }
 }
